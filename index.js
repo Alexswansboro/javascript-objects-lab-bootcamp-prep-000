cec9f0100369699923867dd28a46468c8caea47d
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-26 20:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, value) {
- return Object.assign({[key]: value}, object)
+ return Object.assign({}, object, {value })
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
  var newobj = {}
