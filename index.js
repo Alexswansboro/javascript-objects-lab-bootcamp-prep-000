82689c4d94e9a6d3ea4ccd22cf4776281bffb086
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-26 21:13:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var newObj = Object.assign({}, object)
 delete newObj[key]
 return newObj
 }
-function destructivelyDeleteFromObjectByKey(object, value) {
+function destructivelyDeleteFromObjectByKey(object, key) {
 delete obj[key]
 return obj
 }
