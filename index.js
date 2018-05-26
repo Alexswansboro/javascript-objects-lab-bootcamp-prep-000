81c3ed318e0867b1b5ed164ee4ac1aa886c3185c
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-26 19:46:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {}
+var obj = {prop: 1}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
+ 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object [key] = value;
+  return object
+}
+function deleteFromObjectByKey(object, value) {
+ var newobj = 
+ return Object.assign({}, object, {})
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+ var newobj = {}
+  return newobj
+}
